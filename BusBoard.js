2a6c7363f9880a90e1fc6631d35ca9966b88f48d
RBletso <138777363+RBletso@users.noreply.github.com>
2023-07-06 15:20:22 +0100

    Resolve conflicts

---
+++ BusBoard.js
@@ -0,0 +1,9 @@
+// import fetch from 'node-fetch'
+
+// async function getArrivals(stopCode) {
+//     const arrivalsResponse = await fetch(https://api.tfl.gov.uk/StopPoint/{id}/ArrivalDepartures?lineIds={lineIds})
+//     do {
+//         const
+//     } while (condition); arrivalsPredictions = await response.json();
+
+//     return arrivalsPredictions;
