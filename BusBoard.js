44b67e49b614a0a5713717ef8347bbd09866ac68
RBletso <138777363+RBletso@users.noreply.github.com>
2023-07-07 11:41:47 +0100

    hello

--- BusBoard.js
+++ BusBoard.js
@@ -1,26 +1,36 @@
-// import fetch from 'node-fetch'
-
-// async function getArrivals(stopCode) {
-//     const arrivalsResponse = await fetch(https://api.tfl.gov.uk/StopPoint/{id}/ArrivalDepartures?lineIds={lineIds})
-//     do {
-//         const
-//     } while (condition); arrivalsPredictions = await response.json();
-
-//     return arrivalsPredictions;
-
 import fetch from 'node-fetch';
 
-const stopCode = "490008660N";
+// Get StopCode
+
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin
+    output: process.stdout
+})
+
+rl.question('Enter the bus stop code: ', (stopCode) => {
+    console.log(`You entered bus stop code: ${stopCode}`);
+    // Perform further operations with the stop code
+    rl.close();
+  });
+
 const url = `https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals?`
 
 async function busBoard() {
     const response = await fetch(url);
     const data = await response.json();
     const dataArray = [];
+    let counter = 0;
 
     for (let i = 0; i < data.length; i++) {
         console.log(`Bus ${data[i].lineName} is arriving in ${data[i].timeToStation} seconds.`)
         dataArray.push([data[i].lineName, data[i].timeToStation])
+    }
+
+    counter++;
+    if (counter === 5){
+        break;
     }
 
     dataArray.sort(function(a,b) {
