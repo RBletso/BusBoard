ad277b6dbdf45174956ec2ead32cabe413022ca7
RBletso <138777363+RBletso@users.noreply.github.com>
2023-07-07 14:45:29 +0100

    hello2

--- BusBoard.mjs
+++ BusBoard.mjs
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 // Get StopCode
 
-const readline = require('readline')
+import readline from 'readline';
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -11,11 +11,8 @@ const rl = readline.createInterface({
 
 rl.question('Enter the bus stop code: ', (stopCode) => {
     console.log(`You entered bus stop code: ${stopCode}`);
-    // Perform further operations with the stop code
-    rl.close();
-  });
-
-const url = `https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals?`
+    
+    const url = `https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals?s`
 
 async function busBoard() {
     const response = await fetch(url);
@@ -24,8 +21,8 @@ async function busBoard() {
     let counter = 0;
 
         for (let i = 0; i < data.length; i++) {
-           console.log(`Bus ${data[i].lineName} is arriving in ${data[i].timeToStation} seconds.`)
-           dataArray.push([data[i].lineName, data[i].timeToStation])
+           //console.log(`Bus ${data[i].lineName} is arriving in ${data[i].timeToStation} seconds to ${data[i].destinationName}.`)
+           dataArray.push([data[i].lineName, data[i].timeToStation, data[i].destinationName])
         
            counter++;
            if (counter === 5){
@@ -37,7 +34,13 @@ async function busBoard() {
         return a[1]-b[1];
     });
 
-    dataArray.forEach(element => console.log(`Bus ${element[0]} is arriving in ${element[1]} seconds`));
+    dataArray.forEach(element => console.log(`Bus ${element[0]} is arriving in ${element[1]} seconds to ${element[2]}`));
+
 }
 
 busBoard();
+    
+rl.close();
+    console.log(stopCode)
+  });
+
